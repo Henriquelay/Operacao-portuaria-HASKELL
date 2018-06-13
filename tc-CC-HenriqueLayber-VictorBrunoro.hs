83b10049425f48941780e148c687322b62c9ebdc
Henriquelay <2018103824@labgrad.inf.ufes.br>
2018-06-13 17:27:44 -0300

    Teste de GIT

--- tc-CC-HenriqueLayber-VictorBrunoro.hs
+++ tc-CC-HenriqueLayber-VictorBrunoro.hs
@@ -25,3 +25,7 @@ atendido navio berco infoPorto = if tempoNavio navio berco infoPorto >= abs(thir
                                 else False
 
 filaNavios listaNavios = [ listaNavios!!x | x<-[] ]  --sort??? Slides aula 15
+teste
+asdasds
+aaaa
+aaa
